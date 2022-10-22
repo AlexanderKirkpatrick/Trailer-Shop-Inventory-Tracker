2dbf58e1b34a51b9e2146e24d00dc537e6bd8faa
Alexander <knibz@protonmail.com>
2022-10-22 18:00:12 -0400

    add function and proptypes

--- src/components/EditTrailerForm.js
+++ src/components/EditTrailerForm.js
@@ -1,1 +1,34 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditTrailerForm(props){
+  const { trailer } = props;
+
+  function handleEditTrailerFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTrailer({
+      name: event.target.name.value, 
+      price: parseInt(event.target.price.value), 
+      quantity: parseInt(event.target.quantity.value),
+      description: event.target.description.value,
+      imgURL: event.target.imgURL.value, 
+      id: trailer.id}
+      );
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleEditTrailerFormSubmission}
+        buttonText="Update Trailer" />
+    </React.Fragment>
+  );
+}
+
+EditTrailerForm.propTypes = {
+  trailer: PropTypes.object,
+  onEditTrailer: PropTypes.func
+};
+
 export default EditTrailerForm;
