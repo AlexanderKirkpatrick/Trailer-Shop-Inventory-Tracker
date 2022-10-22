6ed653d658576439a158cb1254d164aeb05b29d8
Alexander <knibz@protonmail.com>
2022-10-22 17:58:43 -0400

    add function, proptypes, and inputs for forms

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,1 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+      <input
+          type='text'
+          name='name'
+          placeholder='Trailer Name' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price' />
+          <input
+          type='text'
+          name='quantity'
+          placeholder='Quantity' />
+        <textarea
+          name='description'
+          placeholder='Description' />
+        <input
+          type='text'
+          name='imgURL'
+          placeholder='Image URL' />  
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string.isRequired
+};
+
 export default ReusableForm;
